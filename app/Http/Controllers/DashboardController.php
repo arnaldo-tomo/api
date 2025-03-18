9455f2a10050c6f938ea8775eabf83167ff2d4c2
Arnaldo tomo <arnaldotomo@gmail.com>
2025-03-18 11:13:31 +0200

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Entry;
 use App\Models\Expense;
+use App\Models\User;
 use Carbon\Carbon;
 
 class DashboardController extends Controller
@@ -18,19 +19,22 @@ class DashboardController extends Controller
         $currentYear = Carbon::now()->year;
 
         // Calcular o total de entradas do mês corrente
-        $totalEntries = Entry::where('user_id', $userId)
-                            ->whereYear('created_at', $currentYear)
-                            ->whereMonth('created_at', $currentMonth)
-                            ->sum('amount');
+        // $totalEntries = Entry::where('user_id', $userId)
+        //                     ->whereYear('created_at', $currentYear)
+        //                     ->whereMonth('created_at', $currentMonth)
+        //                     ->sum('amount');
+
+        // Recuperar o saldo do usuário
+        $totalEntries = User::find($userId);
 
         // Calcular o total de gastos do mês corrente
         $totalExpenses = Expense::where('user_id', $userId)
-                                ->whereYear('created_at', $currentYear)
-                                ->whereMonth('created_at', $currentMonth)
-                                ->sum('amount');
+            ->whereYear('created_at', $currentYear)
+            ->whereMonth('created_at', $currentMonth)
+            ->sum('amount');
 
         return response()->json([
-            'total_entries' => $totalEntries,
+            'total_entries' => $totalEntries->saldo,
             'total_expenses' => $totalExpenses
         ]);
     }
